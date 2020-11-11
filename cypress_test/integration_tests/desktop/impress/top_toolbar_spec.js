746e43e0e2df25df424136cde4ba71aec964a0f3
Thais Vieira <thais.vieira@collabora.com>
2020-11-11 10:31:53 -0300

    impress/top_toolbar: add cypress tests for clear direct formating, italic, underline, strikethrough, font color, and highlightcolor on text shape.
    Change-Id: I7cf9e31beba87cd12d562b0e899b83c895ba68d7
    Signed-off-by: Thais Vieira <thais.vieira@collabora.com>

--- cypress_test/integration_tests/desktop/impress/top_toolbar_spec.js
+++ cypress_test/integration_tests/desktop/impress/top_toolbar_spec.js
@@ -29,6 +29,66 @@ describe('Top toolbar tests.', function() {
 			.should('have.attr', 'font-weight', '700');
 	});
 
+	it('Apply italic on text shape.', function() {
+		cy.get('#tb_editbar_item_italic')
+			.click();
+
+		impressHelper.triggerNewSVGForShapeInTheCenter();
+
+		cy.get('.leaflet-pane.leaflet-overlay-pane g.Page .TextParagraph')
+			.should('have.attr', 'font-weight', '400');
+	});
+
+	it('Apply underline on text shape.', function() {
+		cy.get('#tb_editbar_item_underline')
+			.click();
+
+		impressHelper.triggerNewSVGForShapeInTheCenter();
+
+		cy.get('.leaflet-pane.leaflet-overlay-pane g.Page .TextParagraph')
+			.should('have.attr', 'font-weight', '400');
+	});
+
+	it('Apply strikethrough on text shape.', function() {
+		cy.get('#tb_editbar_item_strikeout')
+			.click();
+
+		impressHelper.triggerNewSVGForShapeInTheCenter();
+
+		cy.get('.leaflet-pane.leaflet-overlay-pane g.Page .TextParagraph')
+			.should('have.attr', 'font-weight', '400');
+	});
+
+	it('Apply clear direct formating on text shape.', function() {
+		cy.get('#tb_editbar_item_reset')
+			.click();
+
+		impressHelper.triggerNewSVGForShapeInTheCenter();
+
+		cy.get('.leaflet-pane.leaflet-overlay-pane g.Page .TextParagraph')
+			.should('have.attr', 'font-weight', '400');
+	});
+
+	it('Apply font color on text shape.', function() {
+		cy.get('#tb_editbar_item_backcolor')
+			.click();
+
+		impressHelper.triggerNewSVGForShapeInTheCenter();
+
+		cy.get('.leaflet-pane.leaflet-overlay-pane g.Page .TextParagraph')
+			.should('have.attr', 'font-weight', '400');
+	});
+
+	it('Apply highlight color on text shape.', function() {
+		cy.get('#tb_editbar_item_backcolor')
+			.click();
+
+		impressHelper.triggerNewSVGForShapeInTheCenter();
+
+		cy.get('.leaflet-pane.leaflet-overlay-pane g.Page .TextParagraph')
+			.should('have.attr', 'font-weight', '400');
+	});
+
 	it('Apply left/right alignment on text seleced text.', function() {
 		cy.get('.leaflet-pane.leaflet-overlay-pane g.Page .TextParagraph .TextPosition')
 			.should('have.attr', 'x', '1400');
